54246bd2da91a68a0002002562f81d1e1023f74b
Roey Berman <roey.berman@forter.com>
2021-01-24 15:07:33 +0200

    Modify interface

--- src/lib.rs
+++ src/lib.rs
@@ -5,20 +5,36 @@ mod machines;
 mod pollers;
 pub mod protos;
 
-use protos::coresdk::{CompleteSdkTaskReq, CompleteSdkTaskResp, PollSdkTaskReq, PollSdkTaskResp};
+use protos::coresdk::{CompleteSdkTaskReq, PollSdkTaskResp, RegistrationReq};
 
-type Result<T, E = SDKServiceError> = std::result::Result<T, E>;
+pub type Result<T, E = SDKServiceError> = std::result::Result<T, E>;
 
 // TODO: Should probably enforce Send + Sync
 #[async_trait::async_trait]
 pub trait CoreSDKService {
-    async fn poll_sdk_task(&self, req: PollSdkTaskReq) -> Result<PollSdkTaskResp>;
-    async fn complete_sdk_task(&self, req: CompleteSdkTaskReq) -> Result<CompleteSdkTaskResp>;
+    async fn poll_sdk_task(&self) -> Result<PollSdkTaskResp>;
+    async fn complete_sdk_task(&self, req: CompleteSdkTaskReq) -> Result<()>;
+    async fn register_implementations(&self, req: RegistrationReq) -> Result<()>;
+}
+
+pub struct CoreSDKInitOptions {
+    queue_name: String,
+    max_concurrent_workflow_executions: u32,
+    max_concurrent_activity_executions: u32,
+}
+
+unsafe impl Send for CoreSDKInitOptions {}
+unsafe impl Sync for CoreSDKInitOptions {}
+
+pub fn init_sdk(opts: CoreSDKInitOptions) -> Result<Box<dyn CoreSDKService>> {
+    Err(SDKServiceError::Unknown {})
 }
 
 #[derive(thiserror::Error, Debug)]
 pub enum SDKServiceError {
-    // tbd
+    #[error("Unknown service error")]
+    Unknown,
+    // more errors TBD
 }
 
 #[cfg(test)]
