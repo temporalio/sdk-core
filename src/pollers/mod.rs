d3e351fd2188dacde481889002ade11b848fa95b
Vitaly <Vitaly.Arbuzov@gmail.com>
2021-02-05 15:59:09 -0800

    Adding an API for workflow creation and makeing gateway accessible in (#39)

--- src/pollers/mod.rs
+++ src/pollers/mod.rs
@@ -1,8 +1,10 @@
 use std::time::Duration;
 
 use crate::machines::ProtoCommand;
+use crate::protos::temporal::api::common::v1::WorkflowType;
 use crate::protos::temporal::api::workflowservice::v1::{
     RespondWorkflowTaskCompletedRequest, RespondWorkflowTaskCompletedResponse,
+    StartWorkflowExecutionRequest, StartWorkflowExecutionResponse,
 };
 use crate::{
     protos::temporal::api::enums::v1::TaskQueueKind,
@@ -11,10 +13,11 @@ use crate::{
     protos::temporal::api::workflowservice::v1::{
         PollWorkflowTaskQueueRequest, PollWorkflowTaskQueueResponse,
     },
-    PollWorkflowTaskQueueApi, RespondWorkflowTaskCompletedApi, Result,
+    PollWorkflowTaskQueueApi, RespondWorkflowTaskCompletedApi, Result, StartWorkflowExecutionApi,
 };
 use tonic::{transport::Channel, Request, Status};
 use url::Url;
+use uuid::Uuid;
 
 /// Options for the connection to the temporal server
 #[derive(Clone, Debug)]
@@ -76,9 +79,19 @@ pub struct ServerGateway {
     pub opts: ServerGatewayOptions,
 }
 
+pub trait ServerGatewayApis:
+    PollWorkflowTaskQueueApi + RespondWorkflowTaskCompletedApi + StartWorkflowExecutionApi
+{
+}
+
+impl<T> ServerGatewayApis for T where
+    T: PollWorkflowTaskQueueApi + RespondWorkflowTaskCompletedApi + StartWorkflowExecutionApi
+{
+}
+
 #[async_trait::async_trait]
 impl PollWorkflowTaskQueueApi for ServerGateway {
-    async fn poll(&self, task_queue: &str) -> Result<PollWorkflowTaskQueueResponse> {
+    async fn poll_workflow_task(&self, task_queue: &str) -> Result<PollWorkflowTaskQueueResponse> {
         let request = PollWorkflowTaskQueueRequest {
             namespace: self.opts.namespace.to_string(),
             task_queue: Some(TaskQueue {
@@ -100,7 +113,7 @@ impl PollWorkflowTaskQueueApi for ServerGateway {
 
 #[async_trait::async_trait]
 impl RespondWorkflowTaskCompletedApi for ServerGateway {
-    async fn complete(
+    async fn complete_workflow_task(
         &self,
         task_token: Vec<u8>,
         commands: Vec<ProtoCommand>,
@@ -122,15 +135,57 @@ impl RespondWorkflowTaskCompletedApi for ServerGateway {
     }
 }
 
+#[async_trait::async_trait]
+impl StartWorkflowExecutionApi for ServerGateway {
+    async fn start_workflow(
+        &self,
+        namespace: &str,
+        task_queue: &str,
+        workflow_id: &str,
+        workflow_type: &str,
+    ) -> Result<StartWorkflowExecutionResponse> {
+        let request_id = Uuid::new_v4().to_string();
+
+        Ok(self
+            .service
+            .clone()
+            .start_workflow_execution(StartWorkflowExecutionRequest {
+                namespace: namespace.to_string(),
+                workflow_id: workflow_id.to_string(),
+                workflow_type: Some(WorkflowType {
+                    name: workflow_type.to_string(),
+                }),
+                task_queue: Some(TaskQueue {
+                    name: task_queue.to_string(),
+                    kind: 0,
+                }),
+                request_id,
+                ..Default::default()
+            })
+            .await?
+            .into_inner())
+    }
+}
+
 #[cfg(test)]
 mockall::mock! {
-    pub(crate) ServerGateway {}
+    pub ServerGateway {}
     #[async_trait::async_trait]
     impl PollWorkflowTaskQueueApi for ServerGateway {
-        async fn poll(&self, task_queue: &str) -> Result<PollWorkflowTaskQueueResponse>;
+        async fn poll_workflow_task(&self, task_queue: &str) -> Result<PollWorkflowTaskQueueResponse>;
     }
     #[async_trait::async_trait]
     impl RespondWorkflowTaskCompletedApi for ServerGateway {
-        async fn complete(&self, task_token: Vec<u8>, commands: Vec<ProtoCommand>) -> Result<RespondWorkflowTaskCompletedResponse>;
+        async fn complete_workflow_task(&self, task_token: Vec<u8>, commands: Vec<ProtoCommand>) -> Result<RespondWorkflowTaskCompletedResponse>;
+    }
+    #[async_trait::async_trait]
+    impl StartWorkflowExecutionApi for ServerGateway {
+        async fn start_workflow(
+            &self,
+            namespace: &str,
+            task_queue: &str,
+            workflow_id: &str,
+            workflow_type: &str,
+        ) -> Result<StartWorkflowExecutionResponse>;
     }
 }
