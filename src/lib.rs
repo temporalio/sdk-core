939647600ac1d0fb06759bd831d6ab4ee5a724ad
Spencer Judge <spencer@temporal.io>
2021-03-30 12:34:34 -0700

    Fix broken doc link

--- src/lib.rs
+++ src/lib.rs
@@ -100,7 +100,7 @@ pub trait Core: Send + Sync {
     fn server_gateway(&self) -> Arc<dyn ServerGatewayApis>;
 
     /// Eventually ceases all polling of the server. [Core::poll_workflow_task] should be called
-    /// until it returns [PollWfError::ShuttingDown] to ensure that any workflows which are still
+    /// until it returns [PollWfError::ShutDown] to ensure that any workflows which are still
     /// undergoing replay have an opportunity to finish. This means that the lang sdk will need to
     /// call [Core::complete_workflow_task] for those workflows until they are done. At that point,
     /// the lang SDK can end the process, or drop the [Core] instance, which will close the
