4fac7f15e216c27e0c1525250dcb7efa79ea3233
Spencer Judge <spencer@temporal.io>
2021-04-06 15:09:05 -0700

    Update README for more info on logging & update opentracing deps (#83)

--- src/core_tracing.rs
+++ src/core_tracing.rs
@@ -1,18 +1,17 @@
 use itertools::Itertools;
-use once_cell::sync::OnceCell;
-use opentelemetry_jaeger::Uninstall;
-use std::collections::VecDeque;
-use tracing_subscriber::{layer::SubscriberExt, util::SubscriberInitExt, EnvFilter};
+use std::{collections::VecDeque, sync::Once};
+use tracing_subscriber::util::SubscriberInitExt;
+use tracing_subscriber::{layer::SubscriberExt, EnvFilter};
 
-static TRACING_INIT: OnceCell<Uninstall> = OnceCell::new();
+static TRACING_INIT: Once = Once::new();
 
 /// Initialize tracing subscribers and output. Core will not call this itself, it exists here so
 /// that consumers and tests have an easy way to initialize tracing.
 pub fn tracing_init() {
-    TRACING_INIT.get_or_init(|| {
-        let (tracer, uninstall) = opentelemetry_jaeger::new_pipeline()
+    TRACING_INIT.call_once(|| {
+        let tracer = opentelemetry_jaeger::new_pipeline()
             .with_service_name("coresdk")
-            .install()
+            .install_batch(opentelemetry::runtime::Tokio)
             .unwrap();
         let opentelemetry = tracing_opentelemetry::layer().with_tracer(tracer);
         let filter_layer = EnvFilter::try_from_default_env()
@@ -26,7 +25,6 @@ pub fn tracing_init() {
             .with(fmt_layer)
             .try_init()
             .unwrap();
-        uninstall
     });
 }
 
