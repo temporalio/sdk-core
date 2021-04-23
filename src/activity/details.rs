8bd74784f03d54a99fcd7b410d7adae8d885ccff
Vitaly <Vitaly.Arbuzov@gmail.com>
2021-04-22 18:40:57 -0700

    use #[derive(derive_more::Constructor)]

--- src/activity/details.rs
+++ src/activity/details.rs
@@ -1,13 +1,8 @@
 use prost_types::Duration;
 
 /// Contains minimal set of details that core needs to store, while activity is running.
+#[derive(derive_more::Constructor)]
 pub(crate) struct InflightActivityDetails {
     /// Used to calculate aggregation delay between activity heartbeats.
     pub heartbeat_timeout: Option<Duration>,
-}
-
-impl InflightActivityDetails {
-    pub fn new(heartbeat_timeout: Option<Duration>) -> Self {
-        Self { heartbeat_timeout }
-    }
 }
