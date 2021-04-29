51767664cd6fd368d2c43bd162669a30d6dbeb20
Spencer Judge <spencer@temporal.io>
2021-04-28 17:01:19 -0700

    Add concurrent heartbeat cancellation test
    Added test to verify core isn't getting hung up on many concurrent heartbeat cancels.
    Also undo issuing pointless activity task for unknown activities.

--- src/activity/details.rs
+++ src/activity/details.rs
@@ -1,9 +1,11 @@
 use prost_types::Duration;
 
 /// Contains minimal set of details that core needs to store, while activity is running.
-#[derive(derive_more::Constructor)]
+#[derive(derive_more::Constructor, Debug)]
 pub(crate) struct InflightActivityDetails {
     pub activity_id: String,
     /// Used to calculate aggregation delay between activity heartbeats.
     pub heartbeat_timeout: Option<Duration>,
+    /// Set to true if we have already issued a cancellation activation to lang for this activity
+    pub issued_cancel_to_lang: bool,
 }
