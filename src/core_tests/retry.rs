caa596f57dc37cf4efbdcce026c8d20a1ab56144
Spencer Judge <spencer@temporal.io>
2021-11-15 09:27:46 -0800

    Polling needs to specify queue type

--- src/core_tests/retry.rs
+++ src/core_tests/retry.rs
@@ -45,14 +45,16 @@ async fn long_poll_non_retryable_errors() {
         let mut mock_gateway = MockServerGatewayApis::new();
         mock_gateway
             .expect_poll_workflow_task()
-            .returning(move |_| Err(Status::new(code, "non-retryable failure")))
+            .returning(move |_, _| Err(Status::new(code, "non-retryable failure")))
             .times(1);
         mock_gateway
             .expect_poll_activity_task()
             .returning(move |_| Err(Status::new(code, "non-retryable failure")))
             .times(1);
         let retry_gateway = RetryGateway::new(mock_gateway, Default::default());
-        let result = retry_gateway.poll_workflow_task("tq".to_string()).await;
+        let result = retry_gateway
+            .poll_workflow_task("tq".to_string(), false)
+            .await;
         assert!(result.is_err());
         let result = retry_gateway.poll_activity_task("tq".to_string()).await;
         assert!(result.is_err());
@@ -86,11 +88,11 @@ async fn long_poll_retries_forever() {
     let mut mock_gateway = MockServerGatewayApis::new();
     mock_gateway
         .expect_poll_workflow_task()
-        .returning(move |_| Err(Status::new(Code::Unknown, "retryable failure")))
+        .returning(move |_, _| Err(Status::new(Code::Unknown, "retryable failure")))
         .times(50);
     mock_gateway
         .expect_poll_workflow_task()
-        .returning(|_| Ok(Default::default()))
+        .returning(|_, _| Ok(Default::default()))
         .times(1);
     mock_gateway
         .expect_poll_activity_task()
@@ -103,7 +105,9 @@ async fn long_poll_retries_forever() {
 
     let retry_gateway = RetryGateway::new(mock_gateway, Default::default());
 
-    let result = retry_gateway.poll_workflow_task("tq".to_string()).await;
+    let result = retry_gateway
+        .poll_workflow_task("tq".to_string(), false)
+        .await;
     assert!(result.is_ok());
     let result = retry_gateway.poll_activity_task("tq".to_string()).await;
     assert!(result.is_ok());
@@ -116,11 +120,11 @@ async fn long_poll_retries_deadline_exceeded() {
         let mut mock_gateway = MockServerGatewayApis::new();
         mock_gateway
             .expect_poll_workflow_task()
-            .returning(move |_| Err(Status::new(code, "retryable failure")))
+            .returning(move |_, _| Err(Status::new(code, "retryable failure")))
             .times(5);
         mock_gateway
             .expect_poll_workflow_task()
-            .returning(|_| Ok(Default::default()))
+            .returning(|_, _| Ok(Default::default()))
             .times(1);
         mock_gateway
             .expect_poll_activity_task()
@@ -133,7 +137,9 @@ async fn long_poll_retries_deadline_exceeded() {
 
         let retry_gateway = RetryGateway::new(mock_gateway, Default::default());
 
-        let result = retry_gateway.poll_workflow_task("tq".to_string()).await;
+        let result = retry_gateway
+            .poll_workflow_task("tq".to_string(), false)
+            .await;
         assert!(result.is_ok());
         let result = retry_gateway.poll_activity_task("tq".to_string()).await;
         assert!(result.is_ok());
