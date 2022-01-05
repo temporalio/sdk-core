9a0362ed566db1bbb7351a7f81fcd418cd9ee2c4
sw-yx <shawnthe1@gmail.com>
2022-01-05 23:12:45 +0800

    impl more of the state machine, with wf command proto

--- src/machines/upsert_search_attributes_state_machine.rs
+++ src/machines/upsert_search_attributes_state_machine.rs
@@ -1,4 +1,17 @@
 use rustfsm::{fsm, TransitionResult};
+use temporal_sdk_core_protos::{
+    coresdk::{
+        // workflow_activation::FireTimer,
+        // workflow_commands::{UpsertCommandCreated, UpsertCommandRecorded, UpsertSearchAttributesCommand},
+        workflow_commands::{UpsertWorkflowSearchAttributes},
+        HistoryEventId,
+    },
+    temporal::api::{
+        command::v1::Command,
+        enums::v1::{CommandType, EventType},
+        history::v1::{history_event, HistoryEvent, TimerFiredEventAttributes},
+    },
+};
 
 fsm! {
     pub(super) name UpsertSearchAttributesMachine; command UpsertSearchAttributesCommand; error UpsertSearchAttributesMachineError;
@@ -13,6 +26,42 @@ fsm! {
 pub(super) enum UpsertSearchAttributesMachineError {}
 
 pub(super) enum UpsertSearchAttributesCommand {}
+
+
+
+/// Creates a new, scheduled, timer as a [CancellableCommand]
+pub(super) fn upsert_search_attrs(attribs: UpsertWorkflowSearchAttributes) -> NewMachineWithCommand {
+    let (state_machine, add_cmd) = UpsertMachine::new_upsert(attribs);
+    NewMachineWithCommand {
+        command: add_cmd,
+        machine: state_machine.into(),
+    }
+}
+
+impl UpsertMachine {
+    /// Create a new timer and immediately schedule it
+    fn new_upsert(attribs: UpsertWorkflowSearchAttributes) -> (Self, Command) {
+        let mut s = Self::new(attribs);
+        OnEventWrapper::on_event_mut(&mut s, TimerMachineEvents::Schedule)
+            .expect("Upserting search attrs doesn't fail");
+        let cmd = Command {
+            command_type: CommandType::UpsertSearchAttributesCommand as i32,
+            attributes: Some(s.shared_state().attrs.clone().into()),
+        };
+        (s, cmd)
+    }
+
+    fn new(attribs: UpsertWorkflowSearchAttributes) -> Self {
+        Self {
+            state: Created {}.into(),
+            shared_state: SharedState {
+                attrs: attribs,
+                cancelled_before_sent: false,
+            },
+        }
+    }
+}
+
 
 #[derive(Default, Clone)]
 pub(super) struct Created {}
