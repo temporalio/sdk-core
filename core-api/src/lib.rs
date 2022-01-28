b8c6374a1d9f8204d8fb4e717d5966c29e9fa0fc
Spencer Judge <spencer@temporal.io>
2022-01-27 18:31:27 -0800

    This line about shutting down was a lie.
    It can't work that way since it consumes self, there's no chance to
    complete anything that somehow snuck into the buffer.

--- core-api/src/lib.rs
+++ core-api/src/lib.rs
@@ -119,8 +119,7 @@ pub trait Worker: Send + Sync {
     /// Completes shutdown and frees all resources. You should avoid simply dropping workers, as
     /// this does not allow async tasks to report any panics that may have occurred cleanly.
     ///
-    /// This may be called once [Worker::shutdown] resolves. If it has not been called, this
-    /// function will call it and wait for it to complete.
+    /// This should be called once [Worker::shutdown] resolves.
     async fn finalize_shutdown(self);
 }
 
