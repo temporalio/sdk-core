41b9742fb7f47ccc96571cfdae6d86557e23d8ae
Roey Berman <roey.berman@gmail.com>
2022-05-10 16:03:12 -0700

    Fix build

--- bridge-ffi/src/wrappers.rs
+++ bridge-ffi/src/wrappers.rs
@@ -1,5 +1,6 @@
 use log::LevelFilter;
-use std::{net::SocketAddr, str::FromStr};
+use parking_lot::Mutex;
+use std::{net::SocketAddr, str::FromStr, sync::Arc};
 use temporal_sdk_core::{TelemetryOptionsBuilder, Url};
 use temporal_sdk_core_protos::coresdk::bridge;
 
@@ -73,8 +74,8 @@ impl TryFrom<ClientOptions> for temporal_sdk_core::ClientOptions {
         if !req.client_version.is_empty() {
             client_opts.client_version(req.client_version);
         }
-        if !req.static_headers.is_empty() {
-            client_opts.static_headers(req.static_headers);
+        if !req.headers.is_empty() {
+            client_opts.headers(Arc::new(Mutex::new(req.headers)));
         }
         if !req.identity.is_empty() {
             client_opts.identity(req.identity);
