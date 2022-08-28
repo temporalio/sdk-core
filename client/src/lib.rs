b05278dbb1328c654d754772a3e5ce0d90e85ce2
Denis Tsai <denistsai@aetheras.io>
2022-08-28 13:51:02 +0800

    alias tonic::Status

--- client/src/lib.rs
+++ client/src/lib.rs
@@ -12,7 +12,7 @@ mod raw;
 mod retry;
 mod workflow_handle;
 
-pub use tonic::Status;
+pub use tonic::Status as TonicStatus;
 pub use crate::retry::{CallType, RetryClient, RETRYABLE_ERROR_CODES};
 pub use raw::{HealthService, OperatorService, TestService, WorkflowService};
 pub use temporal_sdk_core_protos::temporal::api::{
