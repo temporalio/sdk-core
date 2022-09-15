847382d7c706b3a3981019a5e1b906ced35145be
Chad Retz <chad@temporal.io>
2022-09-15 17:10:50 -0500

    Use SIGIN and waitpid on all Unix processes

--- core/src/ephemeral_server/mod.rs
+++ core/src/ephemeral_server/mod.rs
@@ -89,10 +89,6 @@ impl TemporaliteConfig {
             port,
             args,
             has_test_service: false,
-            // Zombie prevention not needed on Temporalite, it shuts down
-            // properly
-            #[cfg(target_family = "unix")]
-            prevent_zombie_on_unix_shutdown: false,
         })
         .await
     }
@@ -130,8 +126,6 @@ impl TestServerConfig {
             port,
             args,
             has_test_service: true,
-            #[cfg(target_family = "unix")]
-            prevent_zombie_on_unix_shutdown: true,
         })
         .await
     }
@@ -142,8 +136,6 @@ struct EphemeralServerConfig {
     port: u16,
     args: Vec<String>,
     has_test_service: bool,
-    #[cfg(target_family = "unix")]
-    prevent_zombie_on_unix_shutdown: bool,
 }
 
 /// Server that will be stopped when dropped.
@@ -153,8 +145,6 @@ pub struct EphemeralServer {
     /// Whether the target implements the gRPC TestService
     pub has_test_service: bool,
     child: tokio::process::Child,
-    #[cfg(target_family = "unix")]
-    prevent_zombie_on_unix_shutdown: bool,
 }
 
 impl EphemeralServer {
@@ -163,9 +153,7 @@ impl EphemeralServer {
         // TODO(cretz): Offer stdio suppression?
         let child = tokio::process::Command::new(config.exe_path)
             .args(config.args)
-            // We kill this on drop in case of errors or other issues. We still
-            // encourage users to manually shutdown.
-            .kill_on_drop(true)
+            .stdin(std::process::Stdio::null())
             .spawn()?;
         let target = format!("127.0.0.1:{}", config.port);
         let target_url = format!("http://{}", target);
@@ -173,8 +161,6 @@ impl EphemeralServer {
             target,
             has_test_service: config.has_test_service,
             child,
-            #[cfg(target_family = "unix")]
-            prevent_zombie_on_unix_shutdown: config.prevent_zombie_on_unix_shutdown,
         });
 
         // Try to connect every 100ms for 5s
@@ -203,11 +189,8 @@ impl EphemeralServer {
     /// a kill if the child process appears completed, but such a check is not
     /// atomic so a kill could still fail as completed if completed just before
     /// kill.
+    #[cfg(not(target_family = "unix"))]
     pub async fn shutdown(&mut self) -> anyhow::Result<()> {
-        #[cfg(target_family = "unix")]
-        if self.prevent_zombie_on_unix_shutdown {
-            return self.shutdown_prevent_zombie().await;
-        }
         // Only kill if there is a PID
         if self.child.id().is_some() {
             Ok(self.child.kill().await?)
@@ -216,22 +199,26 @@ impl EphemeralServer {
         }
     }
 
+    /// Shutdown the server (i.e. kill the child process). This does not attempt
+    /// a kill if the child process appears completed, but such a check is not
+    /// atomic so a kill could still fail as completed if completed just before
+    /// kill.
     #[cfg(target_family = "unix")]
-    async fn shutdown_prevent_zombie(&mut self) -> anyhow::Result<()> {
+    pub async fn shutdown(&mut self) -> anyhow::Result<()> {
         // For whatever reason, Tokio is not properly waiting on result
-        // immediately after sending kill which is causing defunct zombie
-        // processes to remain in some cases and kill() to hang. Therefore, we
-        // have to send the SIGKILL and wait on the process ourselves using a
-        // low-level call.
+        // after sending kill in some cases which is causing defunct zombie
+        // processes to remain and kill() to hang. Therefore, we are sending
+        // SIGINT and waiting on the process ourselves using a low-level call.
         //
         // WARNING: This is based on empirical evidence starting a Python test
         // run on Linux with Python 3.7 (does not happen on Python 3.10 nor does
         // it happen on Temporalite nor does it happen in Rust integration
         // tests). Do not consider this fixed without running that scenario.
         if let Some(pid) = self.child.id() {
-            self.child.start_kill()?;
+            let nix_pid = nix::unistd::Pid::from_raw(pid as i32);
             Ok(spawn_blocking(move || {
-                nix::sys::wait::waitpid(Some(nix::unistd::Pid::from_raw(pid as i32)), None)
+                nix::sys::signal::kill(nix_pid, nix::sys::signal::Signal::SIGINT)?;
+                nix::sys::wait::waitpid(Some(nix_pid), None)
             })
             .await?
             .map(|_| ())?)
