9cf395b39df76116ca8b57da3c7f459f5e629a37
Yaroslav Taben <yaroslav.taben@gmail.com>
2023-03-14 14:47:41 -0400

    Add support for metrics over UDS (#507)
    Co-authored-by: Spencer Judge <spencer@temporal.io>

--- core/src/telemetry/mod.rs
+++ core/src/telemetry/mod.rs
@@ -41,7 +41,11 @@ use temporal_sdk_core_api::telemetry::{
     CoreLog, CoreTelemetry, Logger, MetricTemporality, MetricsExporter, OtelCollectorOptions,
     TelemetryOptions, TraceExporter,
 };
-use tonic::metadata::MetadataMap;
+use tokio::net::UnixStream;
+use tonic::{
+    metadata::MetadataMap,
+    transport::{Endpoint, Uri},
+};
 use tracing::{Level, Subscriber};
 use tracing_subscriber::{layer::SubscriberExt, EnvFilter, Layer};
 
@@ -238,6 +242,22 @@ pub fn telemetry_init(opts: TelemetryOptions) -> Result<TelemetryInstance, anyho
                     headers,
                     metric_periodicity,
                 }) => runtime.block_on(async {
+                    let exporter = opentelemetry_otlp::new_exporter()
+                        .tonic()
+                        .with_metadata(MetadataMap::from_headers(headers.try_into()?));
+                    let exporter = if url.scheme() == "unix" {
+                        // Workaround the fact that tonic doesn't automatically work w/ UDS schemes
+                        let path = url.path().to_owned();
+                        // The endpoint URL is not used.
+                        let channel = Endpoint::try_from("http://[::]:0")?
+                            .connect_with_connector(tower::service_fn(move |_: Uri| {
+                                UnixStream::connect(path.to_owned())
+                            }))
+                            .await?;
+                        exporter.with_channel(channel)
+                    } else {
+                        exporter.with_endpoint(url.to_string())
+                    };
                     let metrics = opentelemetry_otlp::new_pipeline()
                         .metrics(
                             aggregator,
@@ -246,12 +266,7 @@ pub fn telemetry_init(opts: TelemetryOptions) -> Result<TelemetryInstance, anyho
                         )
                         .with_period(metric_periodicity.unwrap_or_else(|| Duration::from_secs(1)))
                         .with_resource(default_resource())
-                        .with_exporter(
-                            opentelemetry_otlp::new_exporter()
-                                .tonic()
-                                .with_endpoint(url.to_string())
-                                .with_metadata(MetadataMap::from_headers(headers.try_into()?)),
-                        )
+                        .with_exporter(exporter)
                         .build()?;
                     Ok::<_, anyhow::Error>(Some(
                         Box::new(metrics) as Box<dyn MeterProvider + Send + Sync>
