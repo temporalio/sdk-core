9cb38d0db98e4978ea6bb1cae84b07d4b5f54b42
Spencer Judge <spencer@temporal.io>
2024-08-05 10:45:40 -0700

    Make sure to use native roots / add cloud ops API test

--- client/src/lib.rs
+++ client/src/lib.rs
@@ -474,7 +474,7 @@ impl ClientOptions {
     /// Passes it through if TLS options not set.
     async fn add_tls_to_channel(&self, mut channel: Endpoint) -> Result<Endpoint, ClientInitError> {
         if let Some(tls_cfg) = &self.tls_cfg {
-            let mut tls = tonic::transport::ClientTlsConfig::new();
+            let mut tls = tonic::transport::ClientTlsConfig::new().with_native_roots();
 
             if let Some(root_cert) = &tls_cfg.server_root_ca_cert {
                 let server_root_ca_cert = Certificate::from_pem(root_cert);
