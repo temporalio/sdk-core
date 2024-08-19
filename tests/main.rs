cdb176f9b55935b44e27289e5ed7aca218ece21c
Spencer Judge <spencer@temporal.io>
2024-08-19 14:16:52 -0700

    Add a basic cloud TLS test

--- tests/main.rs
+++ tests/main.rs
@@ -19,7 +19,7 @@ mod integ_tests {
     mod worker_tests;
     mod workflow_tests;
 
-    use std::str::FromStr;
+    use std::{env, str::FromStr};
     use temporal_client::WorkflowService;
     use temporal_sdk_core::{
         init_worker, ClientOptionsBuilder, ClientTlsConfig, CoreRuntime, TlsConfig,
@@ -27,9 +27,7 @@ mod integ_tests {
     };
     use temporal_sdk_core_api::worker::WorkerConfigBuilder;
     use temporal_sdk_core_protos::temporal::api::workflowservice::v1::ListNamespacesRequest;
-    use temporal_sdk_core_test_utils::{
-        get_integ_server_options, get_integ_telem_options, init_integ_telem,
-    };
+    use temporal_sdk_core_test_utils::{get_integ_server_options, get_integ_telem_options};
     use url::Url;
 
     // Create a worker like a bridge would (unwraps aside)
@@ -61,37 +59,48 @@ mod integ_tests {
             .await;
     }
 
-    // Manually run to verify tls works against cloud. You will need certs in place in the
-    // indicated directory.
     #[tokio::test]
-    #[ignore]
     async fn tls_test() {
-        init_integ_telem();
-        let root = tokio::fs::read("../.cloud_certs/ca.pem").await.unwrap();
-        let client_cert = tokio::fs::read("../.cloud_certs/client.pem").await.unwrap();
-        let client_private_key = tokio::fs::read("../.cloud_certs/client.key").await.unwrap();
+        let cloud_addr = env::var("TEMPORAL_CLOUD_ADDRESS");
+        let cloud_key = env::var("TEMPORAL_CLIENT_KEY");
+
+        let (cloud_addr, cloud_key) = if let (Ok(c), Ok(ck)) = (cloud_addr, cloud_key) {
+            if ck.is_empty() {
+                return; // secret not present in github, could be a fork, just skip
+            }
+            (c, ck)
+        } else {
+            // Skip the test
+            return;
+        };
+
+        let client_cert = dbg!(env::var("TEMPORAL_CLIENT_CERT"))
+            .expect("TEMPORAL_CLIENT_CERT must be set")
+            .replace("\\n", "\n")
+            .into_bytes();
+        let client_private_key = dbg!(cloud_key.replace("\\n", "\n")).into_bytes();
         let sgo = ClientOptionsBuilder::default()
-            .target_url(Url::from_str("https://spencer.temporal-dev.tmprl.cloud:7233").unwrap())
+            .target_url(Url::from_str(&cloud_addr).unwrap())
             .client_name("tls_tester")
             .client_version("clientver")
             .tls_cfg(TlsConfig {
-                server_root_ca_cert: Some(root),
-                // Not necessary, but illustrates functionality for people using proxies, etc.
-                domain: Some("spencer.temporal-dev.tmprl.cloud".to_string()),
                 client_tls_config: Some(ClientTlsConfig {
                     client_cert,
                     client_private_key,
                 }),
+                ..Default::default()
             })
             .build()
             .unwrap();
         let con = sgo
-            .connect("spencer.temporal-dev".to_string(), None)
+            .connect(
+                env::var("TEMPORAL_CLOUD_NAMESPACE").expect("TEMPORAL_CLOUD_NAMESPACE must be set"),
+                None,
+            )
             .await
             .unwrap();
-        dbg!(con
-            .list_workflow_executions(100, vec![], "".to_string())
+        con.list_workflow_executions(100, vec![], "".to_string())
             .await
-            .unwrap());
+            .unwrap();
     }
 }
