951aef3244280edc5a65e2528a6bacb30a96625e
Divyank Jain <divyank.jain@temporal.io>
2024-08-29 16:46:38 -0400

    derive clone for debugclient

--- core/src/debug_client.rs
+++ core/src/debug_client.rs
@@ -8,20 +8,27 @@ use temporal_sdk_core_protos::temporal::api::history::v1::History;
 const CLIENT_NAME: &str = "temporal-core";
 const CLIENT_VERSION: &str = "0.1.0";
 
-struct DebugClient {
+/// Struct representing the internal debug client
+#[derive(Clone)]
+pub struct DebugClient {
+    /// URL for the local instance of the debugger server
     debugger_url: String,
+
+    /// reqwest::Client to avoid re-creating a client for every request.
     client: reqwest::Client,
 }
 
 impl DebugClient {
-    fn new(url: String) -> DebugClient {
+    /// Create a new instance of a DebugClient with the specified url.
+    pub fn new(url: String) -> DebugClient {
         DebugClient {
             debugger_url: url,
             client: reqwest::Client::new(),
         }
     }
 
-    async fn get_history(&self) -> Result<History, anyhow::Error> {
+    /// Get the history from the instance of the debug plugin server, then return the protobinary if successful.
+    pub async fn get_history(&self) -> Result<History, anyhow::Error> {
         let url = self.debugger_url.clone() + "/history";
         let resp = self
             .client
@@ -35,7 +42,11 @@ impl DebugClient {
         Ok(History::decode(bytes)?) // decode_length_delimited() does not work
     }
 
-    async fn post_wft_started(&self, event_id: &i64) -> Result<reqwest::Response, anyhow::Error> {
+    /// Post to current-wft-started to communicate with debug plugin server
+    pub async fn post_wft_started(
+        &self,
+        event_id: &i64,
+    ) -> Result<reqwest::Response, anyhow::Error> {
         let url = self.debugger_url.clone() + "/current-wft-started";
         Ok(self
             .client
@@ -47,21 +58,14 @@ impl DebugClient {
             .send()
             .await?)
     }
-
-    // debating whether this is necessary at all
-    fn content_length(response: &reqwest::Response) -> Result<u64, String> {
-        match response.content_length() {
-            Some(length) => Ok(length),
-            None => Err("Content length unavailable. The header is unavailable or the response was gzipped.".to_owned())
-        }
-    }
 }
 
 #[cfg(test)]
 mod tests {
-    #[tokio::test]
-    async fn test_debug_client_delete() {
-        let dc = DebugClient::new("http://127.0.0.1:51563".into());
-        dc.get_history().await.unwrap();
-    }
+    // this test wont work without a local instance of the debugger running.
+    // #[tokio::test]
+    // async fn test_debug_client_delete() {
+    //     let dc = DebugClient::new("http://127.0.0.1:51563".into());
+    //     dc.get_history().await.unwrap();
+    // }
 }
