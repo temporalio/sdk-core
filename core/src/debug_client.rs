687ba79e1605b9fc3b5cca7190b371aeeb179b07
Spencer Judge <spencer@temporal.io>
2024-10-14 15:53:17 -0700

    Fix url construction & current wft request

--- core/src/debug_client.rs
+++ core/src/debug_client.rs
@@ -16,9 +16,12 @@ const CLIENT_VERSION: &str = "0.1.0";
 pub struct DebugClient {
     /// URL for the local instance of the debugger server
     debugger_url: Url,
-
-    /// Underlying HTTP client
     client: reqwest::Client,
+}
+
+#[derive(Clone, serde::Serialize)]
+struct WFTStartedMsg {
+    event_id: i64,
 }
 
 impl DebugClient {
@@ -34,7 +37,7 @@ impl DebugClient {
 
     /// Get the history from the instance of the debug plugin server
     pub async fn get_history(&self) -> Result<History, anyhow::Error> {
-        let url = self.debugger_url.as_str().to_owned() + "/history";
+        let url = self.debugger_url.join("history")?;
         let resp = self
             .client
             .get(url)
@@ -52,27 +55,17 @@ impl DebugClient {
         &self,
         event_id: &i64,
     ) -> Result<reqwest::Response, anyhow::Error> {
-        let url = self.debugger_url.as_str().to_owned() + "/current-wft-started";
+        let url = self.debugger_url.join("current-wft-started")?;
         Ok(self
             .client
             .get(url)
             .header("Temporal-Client-Name", CLIENT_NAME)
             .header("Temporal-Client-Version", CLIENT_VERSION)
             .timeout(Duration::from_secs(5))
-            .json(event_id)
+            .json(&WFTStartedMsg {
+                event_id: *event_id,
+            })
             .send()
             .await?)
-    }
-}
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-    // this test wont work without a local instance of the debugger running.
-    #[ignore]
-    #[tokio::test]
-    async fn test_debug_client_delete() {
-        let dc = DebugClient::new("http://127.0.0.1:51563".into());
-        dc.get_history().await.unwrap();
     }
 }
