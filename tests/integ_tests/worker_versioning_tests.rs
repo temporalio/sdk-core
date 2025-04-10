715bd608986694e791f22b3d12c0304c87695a89
Spencer Judge <spencer@temporal.io>
2025-04-10 11:34:08 -0700

    Attach deployment options in a few missing spots

--- tests/integ_tests/worker_versioning_tests.rs
+++ tests/integ_tests/worker_versioning_tests.rs
@@ -1,11 +1,13 @@
+use crate::integ_tests::activity_functions::echo;
 use std::time::Duration;
-use temporal_client::{NamespacedClient, WorkflowService};
+use temporal_client::{NamespacedClient, WorkflowOptions, WorkflowService};
+use temporal_sdk::{ActivityOptions, WfContext};
 use temporal_sdk_core_api::worker::{
     WorkerDeploymentOptions, WorkerDeploymentVersion, WorkerVersioningStrategy,
 };
 use temporal_sdk_core_protos::{
     coresdk::{
-        workflow_commands::CompleteWorkflowExecution, workflow_completion,
+        AsJsonPayloadExt, workflow_commands::CompleteWorkflowExecution, workflow_completion,
         workflow_completion::WorkflowActivationCompletion,
     },
     temporal::api::{
@@ -36,8 +38,7 @@ async fn sets_deployment_info_on_task_responses(#[values(true, false)] use_defau
                 use_worker_versioning: true,
                 default_versioning_behavior: VersioningBehavior::AutoUpgrade.into(),
             },
-        ))
-        .no_remote_activities(true);
+        ));
     let core = starter.get_worker().await;
     let client = starter.get_client().await;
 
@@ -128,4 +129,102 @@ async fn sets_deployment_info_on_task_responses(#[values(true, false)] use_defau
         wft_complete.worker_deployment_version,
         format!("{}.1.0", deploy_name)
     );
+}
+
+#[tokio::test]
+async fn activity_has_deployment_stamp() {
+    let wf_name = "activity_has_deployment_stamp";
+    let mut starter = CoreWfStarter::new(wf_name);
+    let deploy_name = format!("deployment-{}", starter.get_task_queue());
+    starter
+        .worker_config
+        .versioning_strategy(WorkerVersioningStrategy::WorkerDeploymentBased(
+            WorkerDeploymentOptions {
+                version: WorkerDeploymentVersion {
+                    deployment_name: deploy_name.clone(),
+                    build_id: "1.0".to_string(),
+                },
+                use_worker_versioning: true,
+                default_versioning_behavior: VersioningBehavior::AutoUpgrade.into(),
+            },
+        ));
+    let mut worker = starter.worker().await;
+    let client = starter.get_client().await;
+    worker.register_wf(wf_name.to_owned(), |ctx: WfContext| async move {
+        ctx.activity(ActivityOptions {
+            activity_type: "echo_activity".to_string(),
+            start_to_close_timeout: Some(Duration::from_secs(5)),
+            input: "hi!".as_json_payload().expect("serializes fine"),
+            ..Default::default()
+        })
+        .await;
+        Ok(().into())
+    });
+    worker.register_activity("echo_activity", echo);
+    let submitter = worker.get_submitter_handle();
+    let shutdown_handle = worker.inner_mut().shutdown_handle();
+
+    let client_task = async {
+        let desc_resp = eventually(
+            async || {
+                client
+                    .get_client()
+                    .clone()
+                    .describe_worker_deployment(DescribeWorkerDeploymentRequest {
+                        namespace: client.namespace().to_string(),
+                        deployment_name: deploy_name.clone(),
+                    })
+                    .await
+            },
+            Duration::from_secs(50),
+        )
+        .await
+        .unwrap()
+        .into_inner();
+
+        client
+            .get_client()
+            .clone()
+            .set_worker_deployment_current_version(SetWorkerDeploymentCurrentVersionRequest {
+                namespace: client.namespace().to_owned(),
+                deployment_name: deploy_name.clone(),
+                version: format!("{}.1.0", deploy_name),
+                conflict_token: desc_resp.conflict_token,
+                ..Default::default()
+            })
+            .await
+            .unwrap();
+
+        submitter
+            .submit_wf(
+                starter.get_wf_id(),
+                wf_name.to_owned(),
+                vec![],
+                WorkflowOptions::default(),
+            )
+            .await
+            .unwrap();
+        starter.wait_for_default_wf_finish().await.unwrap();
+        shutdown_handle();
+    };
+    join!(
+        async {
+            worker.inner_mut().run().await.unwrap();
+        },
+        client_task
+    );
+    let hist = starter.get_history().await;
+    let _activity_completed = hist
+        .events
+        .into_iter()
+        .find_map(|e| {
+            if let Attributes::ActivityTaskCompletedEventAttributes(a) = e.attributes.unwrap() {
+                Some(a)
+            } else {
+                None
+            }
+        })
+        .unwrap();
+    // TODO: Can't actually verify this at the moment as the deployment options are not transferred
+    //   to the event.
 }
