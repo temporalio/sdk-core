52d6adc5855e0d3100f73b56c39206abbd79b736
Dan Davison <dan.davison@temporal.io>
2025-10-08 17:17:36 -0400

    Add Standalone Activity boilerplate

--- client/src/raw.rs
+++ client/src/raw.rs
@@ -1320,6 +1320,135 @@ proxier! {
             r.extensions_mut().insert(labels);
         }
     );
+    (
+        describe_worker,
+        DescribeWorkerRequest,
+        DescribeWorkerResponse,
+        |r| {
+            let labels = namespaced_request!(r);
+            r.extensions_mut().insert(labels);
+        }
+    );
+    (
+        set_worker_deployment_manager,
+        SetWorkerDeploymentManagerRequest,
+        SetWorkerDeploymentManagerResponse,
+        |r| {
+            let labels = namespaced_request!(r);
+            r.extensions_mut().insert(labels);
+        }
+    );
+    (
+        start_activity_execution,
+        StartActivityExecutionRequest,
+        StartActivityExecutionResponse,
+        |r| {
+            let mut labels = namespaced_request!(r);
+            if let Some(ref options) = r.get_ref().options {
+                labels.task_q(options.task_queue.clone());
+            }
+            r.extensions_mut().insert(labels);
+        }
+    );
+    (
+        describe_activity_execution,
+        DescribeActivityExecutionRequest,
+        DescribeActivityExecutionResponse,
+        |r| {
+            let labels = namespaced_request!(r);
+            r.extensions_mut().insert(labels);
+        }
+    );
+    (
+        list_activity_executions,
+        ListActivityExecutionsRequest,
+        ListActivityExecutionsResponse,
+        |r| {
+            let labels = namespaced_request!(r);
+            r.extensions_mut().insert(labels);
+        }
+    );
+    (
+        count_activity_executions,
+        CountActivityExecutionsRequest,
+        CountActivityExecutionsResponse,
+        |r| {
+            let labels = namespaced_request!(r);
+            r.extensions_mut().insert(labels);
+        }
+    );
+    (
+        get_activity_execution_result,
+        GetActivityExecutionResultRequest,
+        GetActivityExecutionResultResponse,
+        |r| {
+            let labels = namespaced_request!(r);
+            r.extensions_mut().insert(labels);
+        }
+    );
+    (
+        request_cancel_activity_execution,
+        RequestCancelActivityExecutionRequest,
+        RequestCancelActivityExecutionResponse,
+        |r| {
+            let labels = namespaced_request!(r);
+            r.extensions_mut().insert(labels);
+        }
+    );
+    (
+        terminate_activity_execution,
+        TerminateActivityExecutionRequest,
+        TerminateActivityExecutionResponse,
+        |r| {
+            let labels = namespaced_request!(r);
+            r.extensions_mut().insert(labels);
+        }
+    );
+    (
+        delete_activity_execution,
+        DeleteActivityExecutionRequest,
+        DeleteActivityExecutionResponse,
+        |r| {
+            let labels = namespaced_request!(r);
+            r.extensions_mut().insert(labels);
+        }
+    );
+    (
+        pause_activity_execution,
+        PauseActivityExecutionRequest,
+        PauseActivityExecutionResponse,
+        |r| {
+            let labels = namespaced_request!(r);
+            r.extensions_mut().insert(labels);
+        }
+    );
+    (
+        unpause_activity_execution,
+        UnpauseActivityExecutionRequest,
+        UnpauseActivityExecutionResponse,
+        |r| {
+            let labels = namespaced_request!(r);
+            r.extensions_mut().insert(labels);
+        }
+    );
+    (
+        reset_activity_execution,
+        ResetActivityExecutionRequest,
+        ResetActivityExecutionResponse,
+        |r| {
+            let labels = namespaced_request!(r);
+            r.extensions_mut().insert(labels);
+        }
+    );
+    (
+        update_activity_execution_options,
+        UpdateActivityExecutionOptionsRequest,
+        UpdateActivityExecutionOptionsResponse,
+        |r| {
+            let labels = namespaced_request!(r);
+            r.extensions_mut().insert(labels);
+        }
+    );
 }
 
 proxier! {
