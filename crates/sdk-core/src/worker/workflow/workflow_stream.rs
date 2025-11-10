8037470c1e51fe10dedc50a0f50a747bd347a4c3
Andrew Yuan <Andrew.Yuan@temporal.io>
2025-11-10 10:58:29 -0800

    add explicit WorkerShutdown input

--- crates/sdk-core/src/worker/workflow/workflow_stream.rs
+++ crates/sdk-core/src/worker/workflow/workflow_stream.rs
@@ -147,6 +147,11 @@ impl WFStream {
                                 });
                                 None
                             }
+                            LocalInputs::WorkerShutdown => {
+                                // Worker shutdown message is used to bump the stream to ensure
+                                // pending workflow activation polls resolve during shutdown
+                                None
+                            }
                         }
                     }
                     WFStreamInput::FailedFetch {
@@ -633,6 +638,7 @@ pub(super) enum LocalInputs {
     RequestEviction(RequestEvictMsg),
     HeartbeatTimeout(String),
     GetStateInfo(GetStateInfoMsg),
+    WorkerShutdown,
 }
 impl LocalInputs {
     fn run_id(&self) -> Option<&str> {
@@ -643,7 +649,7 @@ impl LocalInputs {
             LocalInputs::PostActivation(pa) => &pa.run_id,
             LocalInputs::RequestEviction(re) => &re.run_id,
             LocalInputs::HeartbeatTimeout(hb) => hb,
-            LocalInputs::GetStateInfo(_) => return None,
+            LocalInputs::GetStateInfo(_) | LocalInputs::WorkerShutdown => return None,
         })
     }
 }
