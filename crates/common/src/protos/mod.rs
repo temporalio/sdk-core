139d7b80e884ddfbaa5cab31e4ef296d6b1da8bf
Spencer Judge <spencer@temporal.io>
2026-04-28 16:55:35 -0700

    Fix merge issues

--- crates/common/src/protos/mod.rs
+++ crates/common/src/protos/mod.rs
@@ -3,21 +3,3 @@
 //! that will match the generated structs in this module.
 
 pub use temporalio_common_wasm::protos::*;
-
-#[cfg(feature = "test-utilities")]
-/// Pre-built test histories for common workflow patterns.
-pub mod canned_histories;
-#[cfg(feature = "history_builders")]
-mod history_builder;
-#[cfg(feature = "history_builders")]
-mod history_info;
-#[cfg(feature = "test-utilities")]
-pub mod test_utils;
-
-#[cfg(feature = "history_builders")]
-pub use history_builder::{
-    DEFAULT_ACTIVITY_TYPE, DEFAULT_WORKFLOW_TYPE, TestHistoryBuilder, default_act_sched,
-    default_wes_attribs,
-};
-#[cfg(feature = "history_builders")]
-pub use history_info::HistoryInfo;
